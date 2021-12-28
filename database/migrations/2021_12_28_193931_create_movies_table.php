7336afbee4139d73bd157d2876980a4996323a50
Destru1 <68596367+Destru1@users.noreply.github.com>
2021-12-28 22:09:49 +0200

    Add movie table with crud

---
+++ database/migrations/2021_12_28_193931_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('movie_name');
+            $table->integer('release_year');
+            $table->string('language');
+            $table->boolean('is_age_restricted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
