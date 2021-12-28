0fd8c088b2b063ddd9a612a9ae4246989798ad20
Destru1 <68596367+Destru1@users.noreply.github.com>
2021-12-28 14:14:28 +0200

    Add validations to genre

--- app/Http/Controllers/Admin/GenreCrudController.php
+++ app/Http/Controllers/Admin/GenreCrudController.php
@@ -21,7 +21,7 @@ class GenreCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -33,13 +33,13 @@ class GenreCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
     protected function setupListOperation()
     {
-        CRUD::column('id');
+        //CRUD::column('id');
         CRUD::column('name');
         CRUD::column('created_at');
         CRUD::column('updated_at');
@@ -47,13 +47,13 @@ class GenreCrudController extends CrudController
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
          */
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -61,21 +61,21 @@ class GenreCrudController extends CrudController
     {
         CRUD::setValidation(GenreRequest::class);
 
-        CRUD::field('id');
+       // CRUD::field('id');
         CRUD::field('name');
-        CRUD::field('created_at');
-        CRUD::field('updated_at');
+       // CRUD::field('created_at');
+        //CRUD::field('updated_at');
 
         /**
          * Fields can be defined using the fluent syntax or array syntax:
          * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
          */
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
--- app/Http/Requests/GenreRequest.php
+++ app/Http/Requests/GenreRequest.php
@@ -26,7 +26,7 @@ class GenreRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name' => 'required|min:3|max:255'
         ];
     }
 
@@ -50,7 +50,9 @@ class GenreRequest extends FormRequest
     public function messages()
     {
         return [
-            //
+            'name.required' => 'Name is required',
+            'name.min' => 'Name is too short',
+            'name.max' => 'Name is too long',
         ];
     }
 }
