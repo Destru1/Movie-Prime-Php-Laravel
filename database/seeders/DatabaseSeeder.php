2dfdbcf160644d8623da4e928c55a12197bd4c2b
Destru1 <68596367+Destru1@users.noreply.github.com>
2022-01-22 16:58:08 +0200

    Add seed for user table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -249,5 +249,25 @@ class DatabaseSeeder extends Seeder
              {
                echo("\r\n\e[31m Movie-Producers table is not empty, therefore NOT \r\n]");
              }
+
+
+                if (DB::table('users')->get()->count() === 0) {
+                 DB::table('users')->insert(
+                     [
+                         [
+                             'name' => Str::random(10),
+                             'email' => 'admin@admin.admin',
+                             'password' => Hash::make('admin'),
+                             'created_at' => date('Y-m-d H:i:s'),
+                             'updated_at' => date('Y-m-d H:i:s'),
+                         ]
+                     ]
+                 );
+             }
+             else
+              {
+                 echo "\r\n\e[31m Users table is not empty. \r\n";
+             }
+
     }
 }
